bd9124ac08c2285cf17ef2792153ca714d72362c
RISENje <85687856+RISENje@users.noreply.github.com>
2021-06-11 21:18:40 +0300

    Вторая часть курсовой

---
+++ TicTac/TicTac10x7/TicTac10x7.cpp
@@ -0,0 +1,16 @@
+﻿
+#include "pch.h"
+#include "TicTacManager.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0)); //Чтобы компьютер не повторялся в новой сессии
+
+    TicTacManager manager;
+    while (!manager.isbGameFinished())
+    {
+        manager.MakeMove();
+    }
+
+}
